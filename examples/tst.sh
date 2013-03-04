45f563cd276ec131cdd285631bbd6c2af9909f45
Alberto Alcolea <albertoalcolea@gmail.com>
2013-03-05 00:00:52 +0100

    added folder with examples

---
+++ examples/tst.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# Example script to test meant
+# This script sleep a random time betweem 0 and 6 seconds
+
+sleep $(($RANDOM%7))
--- meant.py
+++ meant.py
@@ -1,5 +1,26 @@
 #! /usr/bin/env python
 # -*- encoding: utf-8 -*-
+#
+# Simple utility to measure the meant execution time of an app and generate 
+# graphs with results.
+#
+# Copyright (c) 2013 Alberto Alcolea (contact@albertoalcolea.com)
+#
+#
+# Meant is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Meant is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Meant.  If not, see <http://www.gnu.org/licenses/>.
+
+
 
 import sys
 import commands
